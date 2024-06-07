0201cfb6f2b61e58c23981d8020d61bb1778a925
Carlos Moreno <cmorenoh2000@alumno.ipn.mx>
2024-06-07 12:48:16 -0600

    Dependencias sweetalert2

--- frontend/src/modules/landing/pages/ChatsGrupales.vue
+++ frontend/src/modules/landing/pages/ChatsGrupales.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="h-screen xl:w-3/4 w-screen absolute bg-[#FFEEE5] flex flex-col items-center">
-    <div class="mt-24 text-center flex flex-col items-center">
+    <div class="mt-14 text-center flex flex-col items-center">
       <h1 class="text-5xl w-auto sm:w-[500px] md:w-[640px] text-stone-500">Chats sugeridos</h1>
       <div class="flex flex-col md:flex-row mt-12 justify-center md:mt-1"></div>
     </div>
