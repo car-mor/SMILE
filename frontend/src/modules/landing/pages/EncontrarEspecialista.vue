194a60d8c0978679278e554c615e9a136278771b
Carlos Moreno <cmorenoh2000@alumno.ipn.mx>
2024-06-07 00:02:25 -0600

    versión inicial frontend

--- frontend/src/modules/landing/pages/EncontrarEspecialista.vue
+++ frontend/src/modules/landing/pages/EncontrarEspecialista.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="h-screen xl:w-3/4 w-screen absolute bg-[#FFEEE5] flex flex-col items-center">
     <div class="mt-24 text-center flex flex-col items-center">
-      <h1 class="text-5xl w-auto sm:w-[500px] md:w-[640px] font-yeseva">
+      <h1 class="text-5xl w-auto sm:w-[500px] md:w-[640px] text-stone-500">
         Encuentra tu especialista y pide tu cita
       </h1>
-      <p class="text-xl mt-6 font-serif w-auto sm:w-[400px] md:w-[480px]">
+      <p class="text-xl mt-6 text-stone-500 w-auto sm:w-[400px] md:w-[480px]">
         Decide de que manera te gustaría agendar tu cita
       </p>
       <div class="flex flex-col md:flex-row mt-12 justify-center md:mt-1"></div>
