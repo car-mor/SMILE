7754d7196cb27eb6493a3e574e0a50636c6aa838
ARCYNTRAR <arcangel557.ac@gmail.com>
2024-06-26 21:28:29 -0600

    Add obtener especialista por modalidad

--- backend/models/MySQL/especialista.js
+++ backend/models/MySQL/especialista.js
@@ -21,6 +21,13 @@ export class EspecialistaModel {
     return especialista[0]
   }
 
+  static async obtenerEspecialistaPorModalidad (modalidad) {
+    const [especialista] = await connectionMySQL.query('select * from especialista where Modalidad = ?;', [modalidad])
+    if (especialista.length === 0) return false
+
+    return especialista
+  }
+
   static async crearEspecialista ({ entrada }) {
     const {
       Nombre,
